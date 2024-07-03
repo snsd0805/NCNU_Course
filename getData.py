96942eeeb07a08cbb562760326e977c8c6711879
Ting-Jun Wang <levi900227@gmail.com>
2024-07-04 01:49:29 +0800

    clean useless code

--- getData.py
+++ getData.py
@@ -114,36 +114,5 @@ def getDepartmentCourses():
     with open(f'歷年課程資料/{SEMESTER}_output.json', 'w') as fp:
         json.dump(courses, fp, ensure_ascii=False)
 
-
-def updateGeneralCourse():
-    with open("歷年課程資料/{}_output.json".format(SEMESTER)) as fp:
-        courses = json.load(fp)
-
-    with open("generalCourse.in") as fp:
-        line = fp.readline()
-        while line:
-            count = 0
-            line = line.split()
-            if len(line) == 2:
-                department = line[1]
-            else:
-                for course in courses:
-                    if course['number'] == line[0]:
-                        course['department'] = department
-                        count += 1
-            # if count == 0 and len(line) != 2:
-            #     print("{} 可能輸入錯誤 - {}".format(line[0], department))
-            line = fp.readline()
-
-    print("還沒有對應到的課程：")
-    for course in courses:
-        if course['department'] == "99, 通識":
-            course['department'] = "99, 通識（未分類）"
-            print("{} {}".format(course['number'], course['name']))
-
-    with open("歷年課程資料/{}_output.json".format(YEAR), "w") as fp:
-        json.dump(courses, fp, ensure_ascii=False)
-
-
 if __name__ == "__main__":
     getDepartmentCourses()
