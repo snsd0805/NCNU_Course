1d3f3594e1e2c46bf9b22348ade8c3c013f1bc53
Ting-Jun Wang <levi900227@gmail.com>
2024-12-24 17:03:20 +0800

    fix: login flow

--- getData.py
+++ getData.py
@@ -5,7 +5,7 @@ import csv
 from bs4 import BeautifulSoup as bs
 import re
 
-SEMESTER = '1131'
+SEMESTER = '1132'
 
 session = requests.Session()
 
@@ -15,8 +15,6 @@ generalCourse = []
 def getDepartmentCourses():
     def getCourseTime(course):
         week_map = dict(zip(['一', '二', '三', '四', '五', '六', '日'], range(1, 8)))
-
-
         # try:
         time_str = course['SemCourseTime']
         print(time_str)
@@ -30,13 +28,21 @@ def getDepartmentCourses():
         # except:
         #    return '另訂'
 
-    response = session.get('https://sis.ncnu.edu.tw/guest?school=ncnu')
+
+    response = session.get('https://sis.ncnu.edu.tw/')
     soup = bs(response.text, 'html.parser')
     token = soup.find('meta', attrs={'name': 'csrf-token'}).get('content')
+    print(f"CSRF TOKEN: {token}")
+
+    response = session.post('https://sis.ncnu.edu.tw/guest', data={'_token': token})
+    soup = bs(response.text, 'html.parser')
+    token = soup.find('meta', attrs={'name': 'csrf-token'}).get('content')
+    print(f"CSRF TOKEN: {token}")
+
     response = session.post('https://sis.ncnu.edu.tw/b09/b09120', data={
         '_token': token,
-        'srh[ACADYear][]': '113',
-        'srh[Semester][]': '1',
+        'srh[ACADYear][]': SEMESTER[:3],
+        'srh[Semester][]': SEMESTER[-1],
         'srh[DayfgID][]': '',
         'srh[ClassTypeID][]': '',
         'srh[CollegeID][]': '',
@@ -58,6 +64,8 @@ def getDepartmentCourses():
         'tb_cancel': '',
         'event': 'search'
     })
+    with open('/tmp/index.html', 'w') as fp:
+        fp.write(response.text)
     
     data_line = ''
     for line in response.text.splitlines():
@@ -85,7 +93,7 @@ def getDepartmentCourses():
             'grade': '0',
             'teacher': course['Teacher'],
             'place': course['ClassRoom'] if course['ClassRoom'] != '' else '另訂',
-            'time': course['SemCourseTime'].replace(',','').replace(' ', ''),# getCourseTime(course),
+            'time': course['SemCourseTime'].replace(',','').replace(' ', '') if course['SemCourseTime'] != "" else '另訂',# getCourseTime(course),
             'credit': course['Credit'],
             'max': course['StdAmtUp'],
             'memo': course['Memo'],
@@ -98,23 +106,31 @@ def getDepartmentCourses():
         '''
 
     # 通識領域
+    fields = [
+        '法政與教育次領域', '國際連結(東南亞)次領域', '工程與科技次領域', '文學與藝術次領域', '歷史哲學與文化次領域', \
+        '生命與科學次領域', '社會創新(在地實踐)次領域', '社經與管理次領域', '淨零永續(綠概念)次領域'
+    ]
     for course in courses:
         if course['department'] == '通識領域課程':
             course['memo'] = course['memo'].replace(',', '，')
             if course['memo'] != '':
-                if '領域' in course['memo']:
+                field = ''
+                for f in fields:
+                    if f in course['memo']:
+                        field = f
+
+                if field:
+                    course['department'] = f"※ 通識－{field}"
                     if '，' in course['memo']: 
                         field, limit = course['memo'].split('，')
-                        course['department'] = f"※ 通識－{field}"
-
                         course['name'] += f'({limit})' 
-                    else:
-                        course['department'] = f"※ 通識－{course['memo']}"
                 else:
-                        course['department'] = f"※ 通識－學校未正常分類之課程"
-
+                    course['department'] = f"※ 通識－學校未正常分類之課程"
             else:
-                course['department'] += '(無分類)'
+                course['department'] = f"※ 通識－學校未正常分類之課程"
+
+    for course in courses:
+        print(course['name'])
 
     with open(f'歷年課程資料/{SEMESTER}_output.json', 'w') as fp:
         json.dump(courses, fp, ensure_ascii=False)
